cbe8bf07c809a43a763e47404aef7e7d8c9c4f5d
RiteshtheSharma <72566311+RiteshtheSharma@users.noreply.github.com>
2023-10-11 17:56:04 +0530

    feat : calculatorObject defined

--- javascript.js
+++ javascript.js
@@ -1,6 +1,40 @@
 let calculatorObject = {
+operand1:null,
+operand2: null,
+operator :null,
+sum: ()=>{this.operand1=this.operand1+this.operand2 ; return this.operand1},
+diff :()=>{this.operand1=this.operand1-this.operand2 ; return this.operand1},
+mul : ()=>{this.operand1=this.operand1*this.operand2 ; return this.operand1},
+mod :()=>{this.operand1=this.operand1%this.operand2 ; return this.operand1},
 
+operation : ()=>{
+    switch(this.operator){
+   case '+':
+    return sum();
+    break;
+    case '-':
+    return diff();
+    break;
+    case '*':
+    return mul();
+    break;
+    case '%':
+    return mod();
+    break;
+    case 'AC':
+    this.operand1=null;
+    this.operand2=null;
+    this.operation=null;
+    return undefined;
+    break;
+    default:
+        return undefined;
+    }
+}
 
+} 
+
+let btnEventManager = {
 
     
 }
